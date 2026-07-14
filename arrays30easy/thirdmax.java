964d4956a01ca5b6a2a824d58f83f22d29279cc7
“ayushss5” <ayushssarkar7@gmail.com>
2026-07-14 11:56:05 +0530

    Fix Third Maximum Number logic

--- arrays30easy/thirdmax.java
+++ arrays30easy/thirdmax.java
@@ -1,4 +1,3 @@
-package arrays30easy;
 import java.util.Scanner;
 
 public class thirdmax {
@@ -11,20 +10,28 @@ public class thirdmax {
         for(int i=0; i<arr.length; i++){
             arr[i] = sc.nextInt();
         }
-        int max1 = Integer.MIN_VALUE;
-        int max2 =Integer.MIN_VALUE;;
-        int max3 = Integer.MIN_VALUE;
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
         for(int i=0; i<arr.length; i++){
+            if(arr[i] == max1 || arr[i] == max2 || arr[i] == max3){
+                continue;
+            }
             if(arr[i]>max1){
+                max3=max2;
                 max2=max1;
                 max1=arr[i];
-                if(arr[i]>max2){
-                    max2=max3;
-                    max2=max1;
-                }
+            }
+            else if(arr[i]>max2){
+                max3=max2;
+                max2=arr[i];
+            }
+            else if(arr[i]>max3){
+                max3=arr[i];
             }
 
         }
+        System.out.println(max3);
 
     }
     
