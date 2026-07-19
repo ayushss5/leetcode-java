884cc2d609e6a6788cdbc4d4fcc22dbd5e9c615b
“ayushss5” <ayushssarkar7@gmail.com>
2026-07-19 15:52:44 +0530

    Add running sum of 1d array solution

---
+++ arrays30easy/runningsumof1darray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class runningsumof1darray {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter values");
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for(int i=0; i<arr.length-1; i++){
+            arr[i+1] = arr[i] +arr[i+1];
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+    
+}
