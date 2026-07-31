7cb48bbc4980fb38cac36cd02d29ba03bfa5f7cc
ayushss5 <ayushssarkar7@gmail.com>
2026-08-01 00:55:39 +0530

    two sum using hashmaps

---
+++ arrays/1.two-sum(2).java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+import java.util.HashMap;
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+                HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int needed = target - nums[i];
+            if(map.containsKey(needed)){
+                return new int[]{map.get(needed),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{};
+        
+        
+    }
+}
+// @lc code=end
+
