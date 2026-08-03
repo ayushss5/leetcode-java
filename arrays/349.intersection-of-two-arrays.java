fe9d42b820123e89b3bd742cef6bdc771d70a559
ayushss5 <ayushssarkar7@gmail.com>
2026-08-03 23:06:02 +0530

    intersection of two arrays

---
+++ arrays/349.intersection-of-two-arrays.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=349 lang=java
+ *
+ * [349] Intersection of Two Arrays
+ */
+
+// @lc code=start
+import java.util.HashSet;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        for (int num : nums1) {
+            set.add(num);
+        }
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                result.add(num);
+            }
+        }
+        int[] ans = new int[result.size()];
+        int i = 0;
+
+        for (int num : result) {
+            ans[i] = num;
+            i++;
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
+
