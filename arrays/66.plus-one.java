742ab056ca109ecc2c5f07e7b7126911b6e9cb7b
ayushss5 <ayushssarkar7@gmail.com>
2026-08-09 23:56:23 +0530

    leetcode plus one solution

---
+++ arrays/66.plus-one.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=66 lang=java
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] ans = new int[digits.length + 1];
+        ans[0] = 1;
+
+        return ans;
+    }
+}
+// @lc code=end
+
