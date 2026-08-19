8c2e60f3702e0d7ca0faed801b01a38c21f9ee86
ayushss5 <ayushssarkar7@gmail.com>
2026-08-20 02:16:16 +0530

    leetcode 709 solution

---
+++ strings/709.to-lower-case.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=709 lang=java
+ *
+ * [709] To Lower Case
+ */
+
+// @lc code=start
+class Solution {
+    public String toLowerCase(String s) {
+        System.out.print("Hello");
+        s.toLowerCase();
+
+        return s;
+
+        
+    }
+}
+// @lc code=end
+
