c59b7d16958ce9a1b70a60fc047ec7b5a80ca099
ayushss5 <ayushssarkar7@gmail.com>
2026-09-01 16:38:37 +0530

    two sum II

--- arrays/167.two-sum-ii-input-array-is-sorted.java
+++ arrays/167.two-sum-ii-input-array-is-sorted.java
@@ -3,27 +3,25 @@
  *
  * [167] Two Sum II - Input Array Is Sorted
  */
-import java.util.*;
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        ArrayList<Integer>arr = new ArrayList<>();
         int left=0;
         int right=numbers.length-1;
         int res[] = new int[2];
 
         while(left<right){
             if(numbers[left]+numbers[right]==target){
-                arr.add(left+1);
-                arr.add(right+1);
-                res[0] = arr.get(0);
-                res[1] = arr.get(1);
+                res[0] = left+1;
+                res[1] = right+1;
                 return res;
             }
             else if(numbers[left]+numbers[right]<target){
                 left++;
-            }
+            }else{
+
             right--;
+            }
         }
         return res;
 
